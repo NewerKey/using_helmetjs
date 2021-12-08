67cc419f50ec411dba84222a02a4b5140871558c
Glitch (hospitable-scalloped-mist) <none>
2021-12-08 17:57:46 +0000

    🛥😽 Checkpoint ./myApp.js:39746186/100

--- myApp.js
+++ myApp.js
@@ -7,8 +7,11 @@ app.use(helmet.hidePoweredBy());
 
 app.use(helmet.frameguard({action: 'deny'}));
 
+app.use(helmet.xssFilter());
 
+app.use(helmet.noSniff());
 
+app.use(helmet.ieNoOpen());
 
 module.exports = app;
 const api = require('./server.js');
