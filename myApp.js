8d60d01196bb76786a5f450d006f955d233e1020
Glitch (hospitable-scalloped-mist) <none>
2021-12-08 17:34:57 +0000

    👄🌄 Checkpoint ./myApp.js:39746186/147

--- myApp.js
+++ myApp.js
@@ -2,12 +2,10 @@ const express = require('express');
 const helmet = require('helmet')
 const app = express();
 
-app.use(helmet());
 
+app.use(helmet.hidePoweredBy());
 
-
-
-
+app.use(helmet.frameguard({action: }));
 
 module.exports = app;
 const api = require('./server.js');
