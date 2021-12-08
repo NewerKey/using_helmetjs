903db0545fb59037775523b08c4d6d6f5b8c53b1
Glitch (hospitable-scalloped-mist) <none>
2021-12-08 18:20:10 +0000

    🐀🎙 Checkpoint ./myApp.js:39746186/61

--- myApp.js
+++ myApp.js
@@ -13,11 +13,13 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
-var ninetyDaysInSeconds  = 7776000 
+var ninetyDaysInSeconds  = 90*24*60*60;
 app.use(helmet.hsts({
   maxAge: ninetyDaysInSeconds, 
   force: true
 }));
+
+app.use(helmet.dnsPrefetchControl());
 
 module.exports = app;
 const api = require('./server.js');
